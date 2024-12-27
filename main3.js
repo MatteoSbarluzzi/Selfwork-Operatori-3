fdf85a6246afcc5ba61c5c1ca293525c13ba6dad
MatteoSbarluzzi <matteosbarluzzi1@gmail.com>
2024-12-27 12:08:18 +0100

    Selfwork Operatori 3 corretto
    Non avevo dato "peso" a "scelti dall'utente"

--- main3.js
+++ main3.js
@@ -1,5 +1,5 @@
-let valore1 = 150;
-let valore2 = 320;
+let valore1 = parseInt(prompt('inserire il primo dato'));
+let valore2 = parseInt(prompt('inserire il secondo dato'));
 console.log(valore1);
 console.log(valore2);
 
@@ -7,10 +7,10 @@ let somma = valore1 + valore2;
 let sottrazione = valore1 - valore2;
 let moltiplicazione = valore1 * valore2;
 let divisione = valore1 / valore2;
-let potenza = Math.pow(valore1, valore2);
+let potenza = valore1 ** valore2;
 
-console.log(`La somma tra i due numeri e' ${somma}`);
-console.log(`La sottrazione tra i due numeri e' ${sottrazione}`);
-console.log(`La moltiplicazione tra i due numeri e' ${moltiplicazione}`);
-console.log(`La divisione tra i due numeri e' ${divisione}`);
-console.log(`La potenza del primo numero elevato al secondo e' ${potenza}`);
+console.log(`La somma tra i due numeri scelti e' ${somma}`);
+console.log(`La sottrazione tra i due numeri scelti e' ${sottrazione}`);
+console.log(`La moltiplicazione tra i due numeri scelti e' ${moltiplicazione}`);
+console.log(`La divisione tra i due numeri scelti e' ${divisione}`);
+console.log(`La potenza del primo numero elevato al secondo da te scelti e' ${potenza}`);

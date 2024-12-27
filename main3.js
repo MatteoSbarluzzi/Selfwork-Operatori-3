0bb7a0dc8134d497df6be5593bb73bbeb65e7ff1
MatteoSbarluzzi <matteosbarluzzi1@gmail.com>
2024-12-27 11:51:49 +0100

    Selfwork Operatori 3

---
+++ main3.js
@@ -0,0 +1,16 @@
+let valore1 = 150;
+let valore2 = 320;
+console.log(valore1);
+console.log(valore2);
+
+let somma = valore1 + valore2;
+let sottrazione = valore1 - valore2;
+let moltiplicazione = valore1 * valore2;
+let divisione = valore1 / valore2;
+let potenza = Math.pow(valore1, valore2);
+
+console.log(`La somma tra i due numeri e' ${somma}`);
+console.log(`La sottrazione tra i due numeri e' ${sottrazione}`);
+console.log(`La moltiplicazione tra i due numeri e' ${moltiplicazione}`);
+console.log(`La divisione tra i due numeri e' ${divisione}`);
+console.log(`La potenza del primo numero elevato al secondo e' ${potenza}`);
